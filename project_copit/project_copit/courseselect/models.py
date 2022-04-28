8a3546ed14652f4b4dfc55d52931681d1a405d63
karitasetna <karitasetna@gmail.com>
2022-04-28 11:49:51 +0000

    prereq

--- project_copit/project_copit/courseselect/models.py
+++ project_copit/project_copit/courseselect/models.py
@@ -12,7 +12,16 @@ class CourseHasPrerequisite(models.Model):
     fromid = models.ForeignKey(Course, on_delete = models.CASCADE, related_name='id1') #foreign key CourseIDa
     toid = models.ForeignKey(Course, on_delete = models.CASCADE, related_name='id2') #foreign key CourseID
 
-    #gera föll td ná í undanfara á x áfanga
+    def get_prerequisite(courseid):
+        all_objects = CourseHasPrerequisite.objects.raw("select * from CourseHasPrerequisite where fromid_id=7")
+        for obj in all_objects:
+            print(obj)
+        prereq_list = all_objects.toid_id
+        
+#        for object in all_objects:
+#            prereq_list = []
+#            prereq_list.append(all_objects.toid_id)
+        return prereq_list
 
     def __str__(self):
         return self.name
@@ -23,3 +32,6 @@ class CourseSemester(models.Model):
 
     def __str__(self):
         return self.name
+
+
+        
