b9ac0864c008cdee1b1f23e10b65f795c13320b6
karitasetna <karitasetna@gmail.com>
2022-05-04 15:21:26 +0000

    ..

--- project_copit/project_copit/project_copit/settings.py
+++ project_copit/project_copit/project_copit/settings.py
@@ -76,17 +76,29 @@ WSGI_APPLICATION = 'project_copit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'RUCourses',
+#        'USER': 'postgres',
+#        'PASSWORD': 'Hjol5129',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+#    }
+#}
+
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'RUCourses',
-        'USER': 'postgres',
-        'PASSWORD': 'Hjol5129',
-        'HOST': '127.0.0.1',
+        'NAME': 'd87bcj3dqnqc43',
+        'USER': 'dnppnphtypjyjo',
+        'PASSWORD': '83c0010f18bf4f9b1430d97df511e030f82dde519650ec8b0b423136d76952ac',
+        'HOST': 'ec2-52-212-228-71.eu-west-1.compute.amazonaws.com',
         'PORT': '5432',
     }
 }
-
 
 
 #DATABASES = {
