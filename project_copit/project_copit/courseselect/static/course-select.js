00d759992c3d62875681414f0d3201917bef1d77
karisteinn99 <71447465+karisteinn99@users.noreply.github.com>
2022-05-04 13:59:39 +0000

    template, static, deleted test folders

--- project_copit/project_copit/courseselect/static/course-select.js
+++ project_copit/project_copit/courseselect/static/course-select.js
@@ -7,24 +7,48 @@ courses.forEach(course => {
         course.classList.add('dragging')
     })
     course.addEventListener('dragend', () => {
-        course.ClassList.remove('dragging')
+        course.classList.remove('dragging')
     })
 });
 
 semesters.forEach(semester => {
     semester.addEventListener('dragover', e => {
-        e.preventDefault()
-        console.log('dragging over')
-        const course = document.querySelector('.dragging')
-        semester.appendChild(course)
+      e.preventDefault()
+      const afterElement = getDragAfterElement(semester, e.clientY)
+      const draggable = document.querySelector('.dragging')
+      if (afterElement == null) {
+        semester.appendChild(draggable)
+      } else {
+        semester.insertBefore(draggable, afterElement)
+      }
     })
-})
+  })
 
-courseSelection.forEach(bigContainer => {
+  courseSelection.forEach(bigContainer => {
     bigContainer.addEventListener('dragover', e => {
-        e.preventDefault()
-        console.log('dragging over')
-        const course = document.querySelector('.dragging')
-        bigContainer.appendChild(course)
+      e.preventDefault()
+      const afterElement = getDragAfterElement(bigContainer, e.clientY)
+      const draggable = document.querySelector('.dragging')
+      if (afterElement == null) {
+        bigContainer.appendChild(draggable)
+      } else {
+        bigContainer.insertBefore(draggable, afterElement)
+      }
     })
-})
+  })
+  
+  function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+    return draggableElements.reduce((closest, child) => {
+      const box = child.getBoundingClientRect()
+      const offset = y - box.top - box.height / 2
+      console.log(offset)
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child }
+      } else {
+        return closest
+      }
+    }, { offset: Number.POSITIVE_INFINITY }).element
+  }
+
+
