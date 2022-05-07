65cc1f82460ff56431a5241f15dd2e5d7e284ae8
karitasetna <karitasetna@gmail.com>
2022-05-07 16:18:25 +0000

    kláraði big check fallið

--- project_copit/project_copit/courseselect/views.py
+++ project_copit/project_copit/courseselect/views.py
@@ -20,57 +20,54 @@ def course_selection(request):
 def big_check(request):
     '''For each HeadRequirement id, goes to SubRequirements, finds corresponding quantity and label_id, 
         gets course_id's (or objects) from CourseHasLabel (with label_id),
-        counts ects of '''
-    result_dict = {} 
+        then compare ects or objects
+        Returns dictionary with results for every headreq and subreq{headreqid:{subreqid:True/False,subreqid:True/False},headreqid:{...}}'''
+    result_dict = {}
     selection_objects = Course.objects.all() #BREYTA SVO Í ACTUAL USER INPUTTIÐ
     for head_requirement in HeadRequirements.objects.all():
-        print("Head requirement:{}".format(head_requirement))
+        result_dict[head_requirement.id] ={}
         for sub_requirement in SubRequirements.objects.all():
-            print("Sub requirement:{}".format(sub_requirement))
             if head_requirement.id == sub_requirement.head_req_id_id:
                 labeled_queryset = sub_requirement.get_courses_with_label() #returns Course objects
-                print(labeled_queryset)
                 labeled_id_list = labeled_queryset.values_list('id') #listi af idum i labeled queryset
-                labeled_selection = selection_objects.filter(pk__in = labeled_id_list) #filtera selectionið með labelinu skila Course objects ÞARF AÐ LAGA
+                labeled_selection = selection_objects.filter(pk__in = labeled_id_list) #filtera selectionið með labelinu skila Course objects
                 if sub_requirement.quantity == -1: #selectionið þarf að innihalda öll objects sem finnast með þessu labeli
                     if labeled_selection.union(labeled_queryset) == labeled_selection:
-                        result_dict[head_requirement.id][sub_requirement.id]=True
+                        result_dict[head_requirement.id][sub_requirement.id]=True #fæ villu þegar ég læt skila bara head_requirement og sub_requirement, væri betra samt
                     else:
                         result_dict[head_requirement.id][sub_requirement.id]=False
                 else: #bera saman selection og labeled queryset einingarnar mv quantity
-                    if labeled_selection.count_ects >= sub_requirement.quantity: #fulfils this sub requirement
+                    if count_ects(labeled_selection) >= sub_requirement.quantity: #fulfils this sub requirement
                         result_dict[head_requirement.id][sub_requirement.id]=True
                     else:
                         result_dict[head_requirement.id][sub_requirement.id]=False
-    #result_dict = {1:{1:True,2:False,3:True},2:{1:False, 2:True, 3:True}...}
-    return result_dict
-#######TESTA#######í terminal:
-from courseselect.models import Course, HeadRequirements, SubRequirements, CourseHasLabel, CourseHasPrerequisite, CourseSemester, Semesters
-from courseselect.checks import count_ects, check_prereq
-result_dict = {} 
-selection_objects = Course.objects.all() #BREYTA SVO Í ACTUAL USER INPUTTIÐ
-for head_requirement in HeadRequirements.objects.all():
-    print("Head requirement:{}".format(head_requirement))
-    for sub_requirement in SubRequirements.objects.all():
-        print("Sub requirement:{}".format(sub_requirement))
-        if head_requirement.id == sub_requirement.head_req_id_id:
-            print("inní if1") #kemst hingað
-            labeled_queryset = sub_requirement.get_courses_with_label() #returns Course objects FÖST HÉR
-            print(labeled_queryset)
-            labeled_id_list = labeled_queryset.values_list('id') #listi af idum i labeled queryset
-            labeled_selection = selection_objects.filter(pk__in = labeled_id_list) #filtera selectionið með labelinu skila Course objects ÞARF AÐ LAGA
-            if sub_requirement.quantity == -1: #selectionið þarf að innihalda öll objects sem finnast með þessu labeli
-                print("inní if2")
-                if labeled_selection.union(labeled_queryset) == labeled_selection:
-                    result_dict[head_requirement.id][sub_requirement.id]=True
-                else:
-                    result_dict[head_requirement.id][sub_requirement.id]=False
-            else: #bera saman selection og labeled queryset einingarnar mv quantity
-                if labeled_selection.count_ects >= sub_requirement.quantity: #fulfils this sub requirement
-                    result_dict[head_requirement.id][sub_requirement.id]=True
-                else:
-                    result_dict[head_requirement.id][sub_requirement.id]=False
-print(result_dict)
+    context = {'requirements': result_dict}
+    return render(request,'check-test.html',context)
+
+# #######TESTA#######í terminal:
+# from courseselect.models import Course, HeadRequirements, SubRequirements, CourseHasLabel, CourseHasPrerequisite, CourseSemester, Semesters
+# from courseselect.checks import count_ects, check_prereq
+# result_dict = {}
+# selection_objects = Course.objects.all() #BREYTA SVO Í ACTUAL USER INPUTTIÐ
+# for head_requirement in HeadRequirements.objects.all():
+#     result_dict[head_requirement.id] ={}
+#     print("Head requirement:{}".format(head_requirement))
+#     for sub_requirement in SubRequirements.objects.all():
+#         print("Sub requirement:{}".format(sub_requirement))
+#         if head_requirement.id == sub_requirement.head_req_id_id:
+#             labeled_queryset = sub_requirement.get_courses_with_label() #returns Course objects
+#             labeled_id_list = labeled_queryset.values_list('id') #listi af idum i labeled queryset
+#             labeled_selection = selection_objects.filter(pk__in = labeled_id_list) #filtera selectionið með labelinu skila Course objects ÞARF AÐ LAGA
+#             if sub_requirement.quantity == -1: #selectionið þarf að innihalda öll objects sem finnast með þessu labeli
+#                 if labeled_selection.union(labeled_queryset) == labeled_selection:
+#                     result_dict[head_requirement.id][sub_requirement.id]=True
+#                 else:
+#                     result_dict[head_requirement.id][sub_requirement.id]=False
+#             else: #bera saman selection og labeled queryset einingarnar mv quantity
+#                 if count_ects(labeled_selection) >= sub_requirement.quantity: #fulfils this sub requirement
+#                     result_dict[head_requirement.id][sub_requirement.id]=True
+#                 else:
+#                     result_dict[head_requirement.id][sub_requirement.id]=False
 
     # check_result_dict = {} #{heareq_id:'result',...}
     # course_objects = Course.objects.all() #þetta verður væntanlega valið hjá user - BREYTA Í DICT
