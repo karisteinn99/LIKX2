df3846d3a79f2b29e431568d437d92525faf632c
karitasetna <karitasetna@gmail.com>
2022-05-09 21:03:48 +0000

    .

--- project_copit/project_copit/courseselect/views.py
+++ project_copit/project_copit/courseselect/views.py
@@ -32,10 +32,11 @@ def big_check(request):
     other_requirements_result_dict = {}
     selection_objects = Course.objects.all() #BREYTA SVO Í ACTUAL USER INPUTTIÐ
     head_requirements_result_dict = check_head_requirements(selection_objects)
-    other_requirements_result_dict["prerequisite check"] = check_prerequisite_by_semester(selection_objects) #ÞEGAR GET FENGIÐ SKIPT EFTIR ÖNNUM
-    other_requirements_result_dict["type check"] = check_course_types(selection_objects) #hvort það sé rétt magn af 12V og 3V
-    other_requirements_result_dict["semester check"] = check_correct_semester(selection_objects) #hvort áfangar séu kenndir á völdu önnunum
-    context = {'head_requirements': head_requirements_result_dict, 'other_requirements': other_requirements_result_dict}
+    #other_requirements_result_dict["prerequisite check"] = check_prerequisite_by_semester(selection_objects) #ÞEGAR GET FENGIÐ SKIPT EFTIR ÖNNUM
+    #other_requirements_result_dict["type check"] = check_course_types(selection_objects) #hvort það sé rétt magn af 12V og 3V
+    #other_requirements_result_dict["semester check"] = check_correct_semester(selection_objects) #hvort áfangar séu kenndir á völdu önnunum
+    #context = {'head_requirements': head_requirements_result_dict, 'other_requirements': other_requirements_result_dict}
+    context = {'head_requirements': head_requirements_result_dict}
     return render(request,'check-test.html',context)
 
     
