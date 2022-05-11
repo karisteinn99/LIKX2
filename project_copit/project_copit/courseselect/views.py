c1b8c6322fe063e92f91df77ece532fb5f617b17
karitasetna <karitasetna@gmail.com>
2022-05-11 21:41:46 +0000

    a

--- project_copit/project_copit/courseselect/views.py
+++ project_copit/project_copit/courseselect/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from courseselect.checks import check_head_requirements, change_dictionary, check_prerequisite_by_semester, check_correct_semester, check_course_types, check_ects_per_semester
-from .models import Course
+from .models import Course, SubRequirements
 
 # form test og course selection smiðað saman
 def course_selection(request):
@@ -23,12 +23,17 @@ def course_selection(request):
                 courses_int.append(int(course))
         semester_dict_output[semester] = courses_int        
 
-    #semester_dict = change_dictionary(semester_dict)
-    #check_correct_semester(semester_dict)
-    #check_course_types(semester_dict)
+    semester_dict = change_dictionary(semester_dict)
     head_requirements_result_dict, other_requirements_result_dict = big_check(semester_dict)
     course_objects = Course.objects.all()
-    context = {'courses': course_objects, 'head_requirements': head_requirements_result_dict, 'other_requirements': other_requirements_result_dict, 'semesters': semester_dict_output, 'courses_in_calender': courses_in_calender}
+
+    sub_req_objects = SubRequirements.objects.all()
+    label_dict = {}
+    for sub_requirement in sub_req_objects:
+        label_dict[sub_requirement] = sub_requirement.get_courses_with_label()
+    print(label_dict)
+
+    context = {'courses': course_objects, 'head_requirements': head_requirements_result_dict, 'other_requirements': other_requirements_result_dict, 'semesters': semester_dict_output, 'courses_in_calender': courses_in_calender, 'label_dict': label_dict}
 
     return render(request, 'course-selection.html', context)
 
@@ -45,7 +50,7 @@ def big_check(selected_objects_by_semester):
     other_requirements_result_dict["prerequisite errors"] = check_prerequisite_by_semester(selected_objects_by_semester) #ÞEGAR GET FENGIÐ SKIPT EFTIR ÖNNUM
     other_requirements_result_dict["semester errors"] = check_correct_semester(selected_objects_by_semester) #hvort áfangar séu kenndir á völdu önnunum
     other_requirements_result_dict[" "] = check_course_types(selected_objects_by_semester) #hvort það sé rétt magn af 12V og 3V
-    other_requirements_result_dict["test"] = check_ects_per_semester(selected_objects_by_semester)
+    other_requirements_result_dict["  "] = check_ects_per_semester(selected_objects_by_semester)
     return head_requirements_result_dict, other_requirements_result_dict
 
     
